069310f05d34a1b084c2cc3eb8b38b06368008c7
emmanuel <emmanuel.roberson3@gmail.com>
2020-10-20 17:19:01 -0500

    Added comments to Node, explaining what it si

--- LinkedLists/Node.h
+++ LinkedLists/Node.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <iostream>
 
+//nodeType is a container for a singular information entry
 template <typename Type>
 struct nodeType
 {
+	// A Type that contains actual stored information entry
 	Type info;
+
+	//Pointer that contains a reference to the next nodeType in a suquence
 	nodeType<Type> * nextLink;
 };
