02a9f2f7df9b452b2c7974bfe161cb6564fd9db5
emmanuel <emmanuel.roberson3@gmail.com>
2020-11-02 16:45:37 -0600

    Added a data structures project. Started on templates queue

---
+++ DataStructures/Queue.h
@@ -0,0 +1,41 @@
+#pragma once
+
+template <class T>
+class Queue
+{
+public:
+
+	//constructor
+	Queue();
+
+	//returns length of queue
+	int Length();
+
+	//tells whether the object is in the queue
+	bool Contains(T);
+
+	//copies the contents of the queue to an array, starting at the specified index
+	void CopyTo(T[], int);
+
+	//removes and returns the object at the beginning of the queue
+	T Dequeue();
+
+	//adds the object to the end of the queue
+	void Enqueue(T);
+
+	//tells whether the object is equal to the current queue
+	bool Equals(Queue<T>);
+
+	//returns the object at the beginning of the queue before removing it
+	T Peek();
+
+
+
+private:
+
+
+
+
+protected:
+
+};
