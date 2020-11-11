b5c7bfd904cab67bb101617bc0e77c77f4df0c2f
emmanuel <emmanuel.roberson3@gmail.com>
2020-11-11 13:23:17 -0600

    Began processess for testing the Queue functions

--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -1,4 +1,18 @@
+#include "queue.h"
+#include <iostream>
+
 int main()
 {
+	//TEST CASES FOR QUEUE<T>
 
+	std::cout << " -- TEST CASES FOR QUEUE -- " << std::endl;
+
+	//instantiate queue class
+	Queue<int> numberQueue(1);
+
+	//print the length to the console
+	std::cout << "Queue length :: " + numberQueue.Length() << std::endl;
+
+
+	return 0;
 }
